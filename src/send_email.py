5a6ecd46cd8d65f667abd2b9ccc9516aa9ab6a65
waste-oxygen <101925681+waste-oxygen@users.noreply.github.com>
2022-09-04 15:54:05 +0800

    Update send_email.py

--- src/send_email.py
+++ src/send_email.py
@@ -4,7 +4,8 @@ from email.mime.text import MIMEText
 
 
 profile = json.load(open("./profile.json"))
-email_address = profile["email_address"]
+email_addressfrom = profile["email_addressfrom"]
+email_addressto = profile["email_addressto"]
 email_authcode = profile["email_authcode"]
 
 
@@ -12,8 +13,8 @@ def send_qqemail_to_myself(message, subject):
     try:
         msg = MIMEText(message)
         msg['Subject'] = subject
-        msg['From'] = email_address
-        msg['To'] = email_address
+        msg['From'] = email_addressfrom
+        msg['To'] = email_addressto
         service = smtplib.SMTP_SSL("smtp.qq.com", 465)
         service.login(msg['From'], email_authcode)
         service.sendmail(msg['From'], msg['To'], msg.as_string())
